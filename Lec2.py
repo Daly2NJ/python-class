24c83f02e18cf5a72bc397532c0fd7ca6dfc4e87
EC2 Default User <ec2-user@ip-172-31-86-3.ec2.internal>
2023-01-26 21:12:32 +0000

    my Lab2 file

---
+++ Lec2.py
@@ -0,0 +1,23 @@
+#print(  type('hello')   ) #single line comment
+#print(  type(123)   )
+#print(  type(123.123)   )
+
+#rint('Hello World')
+#rint('Hello World'.upper())
+#rint('Hello World'.capitalize())
+#rint('Hello World'.split('o'))
+
+#rint('hello' + 'world')
+#rint(1+2)
+#rint(1*2)
+#rint(1-2)
+#rint(1/2)
+my_num = 123
+print(my_num)
+my_num = 234
+print(my_num)
+
+'''
+Lecture 2 of IA241
+Numbers, Strings, and Variables
+'''
---
+++ PC_Chap3.py
@@ -0,0 +1,42 @@
+'''
+Chapter 3 of Python Crash Course
+Introducing Lists
+1/25/2023
+'''
+'''
+bicycles = ['trek', 'cannondale', 'redline', 'specialized']
+print(bicycles)
+print(bicycles[0])
+print(bicycles[0].title())
+print(bicycles[1])
+print(bicycles[3])
+print(bicycles[-1])
+message = f"My first bicycle was a {bicycles[0].title()}"
+print(message)
+'''
+'''
+names = ['Ethan', 'Josh', 'Elijah', 'Joel']
+print(names[0])
+print(names[1])
+print(names[2])
+print(names[3])
+message = f"What's up, {names[0].title()}?"
+print(message)
+message_1 =f"What's up, {names[1].title()}?"
+print(message_1)
+message_2 =f"What's up, {names[2].title()}?"
+print(message_2)
+message_3 =f"What's up, {names[3].title()}?"
+print(message_3)
+'''
+'''
+cars = ['Abarth', 'Alfa Romeo', 'Aston Martin', 'Audi', 'Automobiles Alpine', 'Bandini Automobili', 'Bizzarrini', 'BMW', 'Bugatti', 
+'Caterham Group', 'Cisitalia', 'De Tomaso', 'Ferrari', 'Ginetta Cars', 'Jaguar Cars', 'Jensen Motors', 'Lamborghini', 'Lotus Cars',
+'Maserati', 'MG Motor', 'Norma Auto Concept', 'Pagani', 'Panoz Auto Development', 'Porsche', 'Rimac Automobili', 'Spyker', 'TVR', 
+'Vector Motors']
+
+message = f"If I could drive one car before I die it would be a(n) {cars[12]}!"
+print(message)
+message_1 = f"I hate how the new {cars[23]} cars look."
+print(message_1)
+'''

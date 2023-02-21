a55bf04a305a4f5cd8bb473d40968f8f98e97de3
EC2 Default User <ec2-user@ip-172-31-86-3.ec2.internal>
2023-02-21 22:24:37 +0000

    some notes here.

---
+++ Lab6.py
@@ -0,0 +1,48 @@
+#3.1
+for num in range(0,6):
+    if num != 3:
+        print(num)
+
+#3.2
+result = 1
+for i in range(1,6):
+    result = result * i
+print(result)
+
+#3.3
+result = 0
+for i in range(1,6):
+    result = result + i
+print(result)
+
+#3.4
+result = 1
+for k in range(3,9):
+    result *= k
+print(result)
+
+#3.5
+numerator = 1
+for i in range(1,9):
+    numerator = numerator * i
+denominator = 1
+for i in range(1,4):
+    denominator = denominator * i
+result = numerator / denominator
+print(result)
+
+#3.6
+result = 0 
+for word in 'this is my 6th string'.split():
+    result = result + 1
+#3.7
+my_tweet = {
+"favorite_count":1138, 
+"lang": "en", 
+"coordinates": (-75, 40), 
+"entities": {"hashtags":["Preds", "Pens", "SingIntoSprng"]}
+}
+count = 0
+for i in my_tweet["entities"]["hashtags"]:
+    count += 1
+print(count)
